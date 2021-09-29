dfe07505614c8368732445400408a003da92a43f
luttecervejaria <lucasarcouto1@gmail.com>
2021-09-29 19:40:04 -0300

    Further Correcting

--- android/src/main/kotlin/com/builttoroam/devicecalendar/CalendarDelegate.kt
+++ android/src/main/kotlin/com/builttoroam/devicecalendar/CalendarDelegate.kt
@@ -682,6 +682,7 @@ class CalendarDelegate : PluginRegistry.RequestPermissionsResultListener {
 
         val calId = cursor.getLong(CALENDAR_PROJECTION_ID_INDEX)
         val calSyncId = cursor.getString(CALENDAR_PROJECTION_SYNC_ID_INDEX)
+        if (calSyncId == null) calSyncId = ""
         val displayName = cursor.getString(CALENDAR_PROJECTION_DISPLAY_NAME_INDEX)
         val accessLevel = cursor.getInt(CALENDAR_PROJECTION_ACCESS_LEVEL_INDEX)
         val calendarColor = cursor.getInt(CALENDAR_PROJECTION_COLOR_INDEX)

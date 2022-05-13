eefb16a5fb22a282082d92e5a91ea5f790ed605e
Lucas Couto <lucasarcouto1@gmail.com>
2022-05-13 08:46:28 -0300

    Investigating

--- android/src/main/kotlin/com/builttoroam/devicecalendar/CalendarDelegate.kt
+++ android/src/main/kotlin/com/builttoroam/devicecalendar/CalendarDelegate.kt
@@ -418,7 +418,10 @@ fun retrieveEvent(calendarId: String, eventId: String?, eventIdSync: String?, pe
 
             val eventCursor = contentResolver?.query(eventsUri, EVENT_PROJECTION_RETRIEVE_EVENT, eventSelectionQuery, null, eventSortOrder)
 
-            println("<debug> eventCursor: " + eventCursor)
+            println("<debug> eventsUri: " + eventsUri)
+            println("<debug> EVENT_PROJECTION_RETRIEVE_EVENT: " + EVENT_PROJECTION_RETRIEVE_EVENT)
+            println("<debug> eventSelectionQuery: " + eventSelectionQuery)
+            println("<debug> eventSortOrder: " + eventSortOrder)
 
             var event: Event? = null
 
